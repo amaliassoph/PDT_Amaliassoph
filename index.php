1a34f085ac298b1fd4b27f1b2a7a5fa31aec68d7
amaliassoph <119840589+amaliassoph@users.noreply.github.com>
2023-08-31 20:26:28 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tampilan Sederhana</title>
+</head>
+<body>
+    <header>
+        <h1>Selamat Datang</h1>
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="#">Beranda</a></li>
+            <li><a href="#">Tentang Kami</a></li>
+            <li><a href="#">Kontak</a></li>
+        </ul>
+    </nav>
+    
+    <main>
+        <section>
+            <h2>Artikel Terbaru</h2>
+            <article>
+                <h3>Judul Artikel 1</h3>
+                <p>Ini adalah cuplikan dari artikel 1...</p>
+            </article>
+            <article>
+                <h3>Judul Artikel 2</h3>
+                <p>Ini adalah cuplikan dari artikel 2...</p>
+            </article>
+        </section>
+        
+        <aside>
+            <h2>Info</h2>
+            <p>Informasi tambahan dapat ditampilkan di sini.</p>
+        </aside>
+    </main>
+    
+    <footer>
+        <p>Hak Cipta © 2023. Semua hak dilindungi.</p>
+    </footer>
+</body>
+</html>
